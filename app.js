dacb1afb03655b0c7b7228289f82eb8cdeef1a3a
chajr <chajr@bluetree.pl>
2015-12-12 17:08:34 +0100

    Clear app.js for development

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
 var gpio = require('onoff').Gpio;
 
-var pin5 = new gpio(5, 'out');
-var pin6 = new gpio(6, 'out');
-var pin12 = new gpio(12, 'out');
-var pin13 = new gpio(13, 'out');
-
-pin5.writeSync(1); 
-pin6.writeSync(1); 
-pin12.writeSync(1); 
-pin13.writeSync(1); 
-
