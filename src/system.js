c89e0eeebb3ae8a5a2941d83d3d14dc5c983e1e2
Michal Adamiak <michal.adamiak@performgroup.com>
2016-01-31 20:59:11 +0100

    Created basic structure for system worker

--- src/system.js
+++ src/system.js
@@ -1,7 +1,19 @@
 var exec = require('child_process').exec;
-var startTime = new Date().getTime();
+var log = require('../lib/log');
+var worker = require('../lib/worker');
 var config;
+var name = 'System worker';
 
 exports.launch = function (args, appConfig) {
     config = appConfig;
+    
+    worker.startWorker(
+        collectData,
+        config.get('workers.system.worker_time'),
+        name
+    );
 };
+
+function collectData() {
+    log.logDebug(name + ' heartbeat.');
+}
