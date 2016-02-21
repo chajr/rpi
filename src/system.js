4f7a109aeb813ed964e21024dc3e04833ea9cdce
Michal Adamiak <michal.adamiak@performgroup.com>
2016-02-21 19:54:54 +0100

    Fixed some shell commands

--- src/system.js
+++ src/system.js
@@ -7,20 +7,20 @@ var name = 'System worker';
 
 var commands = {
     date: 'date +"%Y-%m-%d %T"',
-    cpu_utilization: 'top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1}\'',
+    cpu_utilization: 'top -bn2 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1}\' | tail -1', //correct but slow
     memory_free: 'free | grep Mem | awk \'{print $4/$2 * 100.0}\'',
     memory_used: 'free | grep Mem | awk \'{print $3/$2 * 100.0}\'',
     uptime_p: 'uptime -p',
     uptime_s: 'uptime -s',
-    system_load: 'cat /proc/loadavg',
+    system_load: 'cat /proc/loadavg | awk \'{print $1,$2,$3}\'',
     process_number: 'ps -Af --no-headers | wc -l',
-    disk_utilization: 'iostat -d /dev/sda | sed -n "4p"',
-    network_utilization: 'ifstat -i eth0 -q 1 1 | tail -1',
+    disk_utilization: 'iostat -d /dev/sda | sed -n "4p"', //
+    network_utilization: 'ifstat -i eth0 -q 1 1 | tail -1', //
     logged_in_users: 'users',
     logged_in_users_count: 'users | wc -w',
     users_work: 'w -h',
     hostname: 'hostname',
-    ip_internal: 'ip route get 8.8.8.8 | awk "{print $NF; exit}"',
+    ip_internal: 'ip route get 8.8.8.8 | awk \'{print $NF; exit}\'',
     ip_external: 'wget http://ipinfo.io/ip -qO -'
 };
 var data = {};
