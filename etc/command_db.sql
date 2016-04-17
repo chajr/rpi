fe56c2dc93a487b0490af9e9a53e4e6f5f2acea5
chajr <chajr@bluetree.pl>
2016-04-17 13:44:34 +0200

    Updated db structure

--- etc/command_db.sql
+++ etc/command_db.sql
@@ -1,12 +1,14 @@
 CREATE TABLE `commands` (
-  `command_id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `command` TEXT NULL COMMENT '',
-  `date_time` DATETIME NULL COMMENT '',
-  `response_date_time` DATETIME NULL COMMENT '',
-  `executed` TINYINT(1) NULL DEFAULT 0 COMMENT '',
-  `response` TEXT NULL COMMENT '',
-  `exec_time` DATETIME NULL COMMENT '',
-  `error` TINYINT(1) NULL DEFAULT 0 COMMENT '',
-  `host` VARCHAR(50) NULL COMMENT '',
-  `consumed` TINYINT(1) NULL DEFAULT 0 COMMENT '',
-  PRIMARY KEY (`command_id`)  COMMENT '');
+  `command_id` int(11) NOT NULL AUTO_INCREMENT,
+  `command` text,
+  `date_time` datetime DEFAULT NULL,
+  `response_date_time` datetime DEFAULT NULL,
+  `executed` tinyint(1) NOT NULL DEFAULT '0',
+  `response` text,
+  `exec_time` datetime DEFAULT NULL,
+  `error` tinyint(1) NOT NULL DEFAULT '0',
+  `host` varchar(50) DEFAULT NULL,
+  `consumed` tinyint(1) NOT NULL DEFAULT '0',
+  `to_be_exec` datetime DEFAULT NULL,
+  PRIMARY KEY (`command_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin2;
