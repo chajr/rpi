a9478e24007bc41855316dd500181ee0807fe880
chajr <chajr@bluetree.pl>
2016-10-07 21:03:12 +0200

    Implement new version of illuminate handling

--- src/illuminate.js
+++ src/illuminate.js
@@ -4,6 +4,14 @@ var redis = require('../lib/redis.js');
 var launched = false;
 
 exports.launch = function (args, config) {
+    if (config.get('illuminate') === 'old') {
+        oldIlluminate(args, config);
+    } else {
+        newIlluminate(args, config);
+    }
+};
+
+function oldIlluminate(args, config) {
     var pin1 = new gpio(config.get('illuminate_gpio.pin_1'), 'out');
     var pin2 = new gpio(config.get('illuminate_gpio.pin_2'), 'out');
     var pin3 = new gpio(config.get('illuminate_gpio.pin_3'), 'out');
@@ -29,4 +37,34 @@ exports.launch = function (args, config) {
     var message = 'Pin set to: ' + pinVal;
     console.log(message);
     log.logInfo(message);
-};
+}
+
+function newIlluminate(args, config) {
+    var pin1 = new gpio(config.get('illuminate_gpio.pin_1'), 'out');
+    var pin2 = new gpio(config.get('illuminate_gpio.pin_2'), 'out');
+
+    redis.connect();
+
+    if (args[0] === 'on') {
+        try {
+            pin1.writeSync(1);
+            pin2.writeSync(1);
+            console.log(led);
+
+            launched = true;
+            redis.setData('illuminate_status', 'true');
+        } catch(err) {}
+    } else {
+        try {
+            launched = false;
+            redis.setData('illuminate_status', 'false');
+
+            var ledR1 = new gpio(pin1, 'in');
+            var ledR2 = new gpio(pin2, 'in');
+
+            pin1.writeSync(1);
+            pin2.writeSync(1);
+            console.log(led);
+        } catch(err) {}
+    }
+}
