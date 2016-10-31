7dd79d09a737b8ddb588d6ba011931d4ac11d0eb
chajr <chajr@bluetree.pl>
2016-10-31 14:28:47 +0100

    Fixed read CPU utilization

--- proc.sh
+++ proc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-top -bn2 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}' | tail -1 > /var/log/proc.log
+top -bn1 | grep -i "%Cpu" | head -1 | sed "s/.*, *\([0-9]*,.\)%* \(id\|be\).*/\1/;s/,/./" | awk '{print 100 -$1}' > /var/log/proc.log
