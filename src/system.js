f7b8704997b42971025b2a6e1feb57d42c5c22bf
chajr <chajr@bluetree.pl>
2016-11-04 17:54:27 +0100

    Fixed memorry system status

--- src/system.js
+++ src/system.js
@@ -8,8 +8,8 @@ var name = 'System worker';
 var commands = {
     date: 'date +"%Y-%m-%d %T"',
     cpu_utilization: 'cat /var/log/proc.log',
-    memory_free: 'free | grep Mem | awk \'{print $4/$2 * 100.0}\'',
-    memory_used: 'free | grep Mem | awk \'{print $3/$2 * 100.0}\'',
+    memory_free: 'free | grep "Mem\|Pamięć" | awk \'{print $4/$2 * 100.0}\'',
+    memory_used: 'free | grep "Mem\|Pamięć" | awk \'{print $3/$2 * 100.0}\'',
     uptime_p: 'uptime -p',
     uptime_s: 'uptime -s',
     system_load: 'cat /proc/loadavg | awk \'{print $1,$2,$3}\'',
