3f4cbdec36a6ff08894c1b6575655ab71138d280
chajr <chajr@bluetree.pl>
2016-12-30 17:21:11 +0100

    Added les status worker basic configuration

--- setup.sh
+++ setup.sh
@@ -10,16 +10,19 @@ redis-cli set rpia_illuminate_light_1 false
 redis-cli set rpia_illuminate_light_2 false
 redis-cli set rpia_alert_armed false
 redis-cli set rpia_sms_send false
+redis-cli set rpia_error_led false
 redis-cli expire rpia_sms_send 900
 
 sudo touch /var/log/rpi-mc-system.log
 sudo touch /var/log/rpi-mc-autoIlluminate.log
 sudo touch /var/log/rpi-a-display.log
 sudo touch /var/log/rpi-mc-server.log
+sudo touch /var/log/rpi-error-led.log
 sudo chown $USER:$USER /var/log/rpi-mc-system.log
 sudo chown $USER:$USER /var/log/rpi-mc-server.log
 sudo chown $USER:$USER /var/log/rpi-a-display.log
 sudo chown $USER:$USER /var/log/rpi-mc-autoIlluminate.log
+sudo chown $USER:$USER /var/log/rpi-error-led.log
 
 sudo touch /etc/init.d/rpi-mc.sh
 sudo chmod +x /etc/init.d/rpi-mc.sh
