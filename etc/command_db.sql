21f06865849691c30784255046759a6c4b440c47
chajr <chajr@bluetree.pl>
2017-01-06 13:58:09 +0100

    Added system_log table creation

--- etc/command_db.sql
+++ etc/command_db.sql
@@ -1,3 +1,27 @@
+CREATE TABLE `system_log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpu_utilization` decimal(10,2) DEFAULT NULL,
+  `system_load` varchar(45) DEFAULT NULL,
+  `memory_free` float DEFAULT NULL,
+  `memory_used` float DEFAULT NULL,
+  `network_utilization` varchar(45) DEFAULT NULL,
+  `disk_utilization` varchar(45) DEFAULT NULL,
+  `hostname` varchar(45) DEFAULT NULL,
+  `log_time` datetime DEFAULT NULL,
+  `uptime_p` varchar(45) DEFAULT NULL,
+  `uptime_s` varchar(45) DEFAULT NULL,
+  `process_number` int(11) DEFAULT NULL,
+  `logged_in_users` varchar(255) DEFAULT NULL,
+  `logged_in_users_count` int(11) DEFAULT NULL,
+  `users_work` text,
+  `ip_internal` varchar(15) DEFAULT NULL,
+  `ip_external` varchar(15) DEFAULT NULL,
+  `extra` text,
+  `log_server_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  UNIQUE KEY `log_id_UNIQUE` (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=418609 DEFAULT CHARSET=latin2;
+
 CREATE TABLE `commands` (
   `command_id` int(11) NOT NULL AUTO_INCREMENT,
   `command` text,
