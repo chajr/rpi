a6523ee72f08172849bcc0cc654078b319be6dcf
chajr <chajr@bluetree.pl>
2017-01-06 14:55:41 +0100

    Changed app.js to automatically launch command from srcif exists

--- app.js
+++ app.js
@@ -3,52 +3,17 @@
 var config = require('./lib/config');
 var log = require('./lib/log.js');
 var redis = require('./lib/redis.js');
+var fs = require('fs');
 
 var args = process.argv.slice(2);
 var startTime = new Date().getTime();
 var app;
 
-switch (args[0]) {
-    case 'illuminate':
-        app = require('./src/illuminate');
-        break;
-
-    case 'alarm':
-        app = require('./src/alert');
-        break;
-
-    case 'system':
-        app = require('./src/system');
-        break;
-
-    case 'autoIlluminate':
-        app = require('./src/autoIlluminate');
-        break;
-
-    case 'command':
-        app = require('./src/commandConsummer');
-        break;
-
-    case 'info':
-        app = require('./src/info');
-        break;
-
-    case 'display':
-        app = require('./src/display');
-        break;
-
-    case 'picture':
-        app = require('./src/picture');
-        break;
-
-    case 'errorLed':
-        app = require('./src/errorLed');
-        break;
-
-    default:
-        console.log('nothing to run');
-        process.exit(0);
-        break;
+if (fs.existsSync('./src/' + args[0] + '.js')) {
+    app = require('./src/' + args[0]);
+} else {
+    console.log('nothing to run');
+    process.exit(1);
 }
 
 args.splice(0, 1);
