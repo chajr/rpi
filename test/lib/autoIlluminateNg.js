dc8d54646a6c6447505bac2eba4e6e4d965ef915
chajr <chajr@bluetree.pl>
2017-08-31 20:48:25 +0200

    Added basic test file for autoIlluminateNg

--- test/lib/autoIlluminateNg.js
+++ test/lib/autoIlluminateNg.js
@@ -1,13 +1,31 @@
 let Config = require('../../lib/config');
-let Iluminator = require('../../lib/iluminatorNg');
+let autoIlluminateNg = require('../../src/autoIlluminateNg');
 let assert = require('assert');
 
 describe('Test Auto Illuminate Library', function() {
     describe('', function () {
         it('', function () {
-            assert.equal(Iluminator.xor(true, false), true);
-            assert.equal(Iluminator.xor(false, true), true);
+            // assert.equal(Iluminator.xor(true, false), true);
+            // assert.equal(Iluminator.xor(false, true), true);
+            
+            // let redis = new RedisMock();
+            
 
         });
     });
 });
+
+class RedisMock
+{
+    connect () {
+        this.data = [];
+    }
+    
+    getData (key, callback) {
+        callback(this.data[key]);
+    }
+    
+    setData (key, value) {
+        this.data[key] = value;
+    }
+}
